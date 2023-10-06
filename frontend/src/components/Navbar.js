29ca11c29b1347b5e26eee3e4e7152c20682de60
root <root@DESKTOP-U5TE1T7.localdomain>
2023-10-06 20:32:28 +0530

    fixed some typo

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
   });
   return (
     <div className='flex  p-2 justify-between items-center mb-2'>
-      <h1 className='text-[3vmax] font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-green-500'>NftSala</h1>
+      <h1 className='text-[3vmax] font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-green-500'>NftShala</h1>
       <div>
         <Link to="/" className='mx-2 font-semibold border-b-2 hover:border-green-400  p-2 text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-green-500'>Home</Link>
         <Link to="/sellnft" className='mx-2 font-semibold border-b-2 hover:border-green-400  p-2 text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-green-500'>SellNft</Link>
